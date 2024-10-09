a2e5f5a46fc32f637c1ddd775467b394516dede8
Trần Gia Huy <huytgcs170073@fpt.edu.vn>
2024-10-09 22:27:40 +0700

    User_ViewList_Update

---
+++ StoreMMO/StoreMMO.Web/Models/ViewModels/UserUpdateViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreMMO.Web.Models.ViewModels
+{
+    public class UserUpdateViewModel
+    {
+        [BindProperty]
+        [Required(ErrorMessage = "Full Name is required.")]
+        [StringLength(100, ErrorMessage = "Full Name cannot be longer than 100 characters.")]
+        public string FullName { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [BindProperty]
+        [Phone(ErrorMessage = "Invalid Phone Number.")]
+        public string PhoneNumber { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; }
+
+        [BindProperty]
+        [StringLength(250, ErrorMessage = "Address cannot be longer than 250 characters.")]
+        public string Address { get; set; }
+        // Thêm các thuộc tính khác mà bạn cần nhận từ form
+
+
+
+
+    }
+}
