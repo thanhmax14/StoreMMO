15e8e42b8d2b340a80ea4bc7b7692315fb400f2f
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2024-10-09 19:59:00 +0700

    a

---
+++ StoreMMO/StoreMMO.Web/Pages/Seller/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StoreMMO.Web.Pages.Seller
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
