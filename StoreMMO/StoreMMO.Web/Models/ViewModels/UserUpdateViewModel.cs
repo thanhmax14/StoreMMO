f737de31bf3cffb92c1e13d7d59f85cdddf346af
Trần Gia Huy <huytgcs170073@fpt.edu.vn>
2024-10-10 18:32:12 +0700

    updateUser

--- StoreMMO/StoreMMO.Web/Models/ViewModels/UserUpdateViewModel.cs
+++ StoreMMO/StoreMMO.Web/Models/ViewModels/UserUpdateViewModel.cs
@@ -1,33 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 
 namespace StoreMMO.Web.Models.ViewModels
 {
     public class UserUpdateViewModel
     {
-        [BindProperty]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+
         [Required(ErrorMessage = "Full Name is required.")]
         [StringLength(100, ErrorMessage = "Full Name cannot be longer than 100 characters.")]
         public string FullName { get; set; }
 
-        [BindProperty]
+
         [DataType(DataType.Date)]
         [Display(Name = "Date of Birth")]
         public DateTime? DateOfBirth { get; set; }
 
-        [BindProperty]
+
         [Phone(ErrorMessage = "Invalid Phone Number.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Only numbers are allowed.")]
+        [StringLength(10, ErrorMessage = "The phone number must be up to 10 digits.")]
         public string PhoneNumber { get; set; }
 
-        [BindProperty]
+
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
 
-        [BindProperty]
+
         [StringLength(250, ErrorMessage = "Address cannot be longer than 250 characters.")]
         public string Address { get; set; }
-        // Thêm các thuộc tính khác mà bạn cần nhận từ form
+
 
 
 
--- StoreMMO/StoreMMO.Web/Pages/Admin/UpdateUser.cshtml.cs
+++ StoreMMO/StoreMMO.Web/Pages/Admin/UpdateUser.cshtml.cs
@@ -1,9 +1,10 @@
-using BusinessLogic.Services.StoreMMO.Core.User;
+﻿using BusinessLogic.Services.StoreMMO.Core.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StoreMMO.Core.Models;
 using StoreMMO.Core.ViewModels;
+using StoreMMO.Web.Models.ViewModels;
 
 namespace StoreMMO.Web.Pages.Admin
 {
@@ -18,22 +19,29 @@ namespace StoreMMO.Web.Pages.Admin
             _userManager = userManager;
             _userServices = userServices;
         }
-
+        [BindProperty]
+        public UserUpdateViewModel input { get; set; }
         public IEnumerable<UserViewModel> list = new List<UserViewModel>();
         [TempData]
         public string role1 { get; set; }
         [TempData]
         public string role2 { get; set; }
 
+        [TempData]
+        public string success { get; set; }
+        [TempData]
+        public string fail { get; set; }
         public IActionResult OnGet(string userId)
         {
 
             list = this._userServices.GetUserById(userId);
-            var ten = "";
+
             foreach (var item in list)
             {
+                var tem = item.PhoneNumber;
                 if (item.RoleName.Equals("Admin"))
                 {
+
                     role1 = "User";
                     role2 = "Seller";
                 }
@@ -50,6 +58,51 @@ namespace StoreMMO.Web.Pages.Admin
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToPage("Error404");
+            }
+            else
+            {
+                var tem = input;
+                var checkEmail = await this._userManager.FindByEmailAsync(input.Email);
+                if (checkEmail != null)
+                {
+                    checkEmail.FullName = input.FullName;
+                    checkEmail.DateOfBirth = input.DateOfBirth;
+                    checkEmail.PhoneNumber = input.PhoneNumber;
+                    checkEmail.Address = input.Address;
+
+                    if (checkEmail.PasswordHash != input.Password)
+                    {
+                        var resetTokent = await _userManager.GeneratePasswordResetTokenAsync(checkEmail);
+                        var resetPassResult = await _userManager.ResetPasswordAsync(checkEmail, resetTokent, input.Password);
+                        if (!resetPassResult.Succeeded)
+                        {
+                            fail = "Update mat khau that bai";
+                            return RedirectToPage("UpdateUser", new { userID = checkEmail.Id }); // Hiển thị lỗi nếu không thành công
+                        }
+
+                    }
+                    var update = await this._userManager.UpdateAsync(checkEmail);
+                    if (update.Succeeded)
+                    {
+                        success = "Update Thong Tin Thanh cong";
+                    }
+                    else
+                    {
+                        fail = "Update Thong Tin That Bai";
+                    }
+                    return RedirectToPage("UpdateUser", new { userID = checkEmail.Id });
+                }
+                return RedirectToPage("Error404");
+
+            }
+
         }
     }
 }
