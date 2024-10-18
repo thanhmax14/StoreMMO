dc53f556729957bdc4b9bd4f67a929373356b84a
Trần Gia Huy <huytgcs170073@fpt.edu.vn>
2024-10-18 17:34:42 +0700

    Update UserRepository.cs

--- StoreMMO/StoreMMO.Core/Repositories/User/UserRepository.cs
+++ StoreMMO/StoreMMO.Core/Repositories/User/UserRepository.cs
@@ -14,7 +14,7 @@ namespace StoreMMO.Core.Repositories.User
         public IEnumerable<UserViewModel> GetAllUser(bool isDelete)
         {
 
-            string sql = $"SELECT       Users.Id as UserID, Users.FullName, Users.DateOfBirth, Users.Address, Users.IsSeller, Users.UserName, Users.Email, Users.PhoneNumber, Roles.Name as RoleName,Users.PasswordHash, Users.CreatedDate FROM            Users INNER JOIN                         UserRoles ON Users.Id = UserRoles.UserId INNER JOIN                     Roles ON UserRoles.RoleId = Roles.Id where Users.IsDelete = '{isDelete}'";
+            string sql = $"SELECT       Users.Id as UserID, Users.FullName, Users.DateOfBirth, Users.Address, Users.IsSeller, Users.UserName, Users.Email, Users.PhoneNumber, Roles.Name as RoleName,Users.PasswordHash, Users.CreatedDate FROM            Users INNER JOIN                         UserRoles ON Users.Id = UserRoles.UserId INNER JOIN                     Roles ON UserRoles.RoleId = Roles.Id where Users.IsDelete = '{isDelete}'  and  Roles.Name != 'admin'";
             var list = this._context.Database.SqlQueryRaw<UserViewModel>(sql).ToList();
             return list;
         }
