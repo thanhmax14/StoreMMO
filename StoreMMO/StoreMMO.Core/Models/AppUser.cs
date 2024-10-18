fb53b536370a8b13ed212701212c0c39998a9559
Trần Gia Huy <huytgcs170073@fpt.edu.vn>
2024-10-18 22:43:49 +0700

    update appuser

--- StoreMMO/StoreMMO.Core/Models/AppUser.cs
+++ StoreMMO/StoreMMO.Core/Models/AppUser.cs
@@ -1,29 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StoreMMO.Core.Models
 {
-    public class AppUser: IdentityUser
+    public class AppUser : IdentityUser
     {
-		
-		public string? FullName { get; set; }
+
+        public string? FullName { get; set; }
         public DateTime? DateOfBirth { get; set; }
-		public string? Address { get; set; }
+        public string? Address { get; set; }
         public bool IsDelete { get; set; }
         public DateTime? DateDelete { get; set; }
-        public  DateTime? DateRestore { get; set; }
-        public DateTime? CreatedDate { get; set; }   
+        public DateTime? DateRestore { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public DateTime? ModifiedDateUpdateProfile { get; set; }
         public bool IsSeller { get; set; } = false;
         public DateTime? RequestSellerDate { get; set; }
         public DateTime? SellerApprovalDate { get; set; }
-        public string BalanceID { get; set; }
+        public string? BalanceID { get; set; }
         public Balance Balance { get; set; }
     }
 }
