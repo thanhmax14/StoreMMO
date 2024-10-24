cdda85006b1113ccc4c7acda27e7e6619f461de9
cognuhn <congminhn855@gmail.com>
2024-10-24 15:45:21 +0700

    compit to pull code form NGocNGoc  to my branch

--- StoreMMO/BusinessLogic/Services/AutoMapper/AutoMappers.cs
+++ StoreMMO/BusinessLogic/Services/AutoMapper/AutoMappers.cs
@@ -56,6 +56,9 @@ namespace BusinessLogic.Services.AutoMapper
 
             // UserMapper mapping
             CreateMap<AppUser, UserMapper>();
+            // New BalanceMapper mapping
+            CreateMap<Balance, BalanceMapper>()
+                .ForMember(dest => dest.Usermapforbalance, opt => opt.MapFrom(src => src.User)); // Map User to Usermapforbalance
         }
     }
 }
