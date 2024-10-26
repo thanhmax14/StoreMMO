4eb12750c937bae6b2193f8fc18c09404be603f4
Duong Binh Minh - N1 <MINHDBCE170224@FPT.EDU.VN>
2024-10-26 16:09:52 +0700

    button

--- StoreMMO/StoreMMO.Web/Pages/Admin/StoreList.cshtml.cs
+++ StoreMMO/StoreMMO.Web/Pages/Admin/StoreList.cshtml.cs
@@ -45,9 +45,9 @@ namespace StoreMMO.Web.Pages.Admin
 
             // Chuyển hướng lại trang danh sách danh mục ẩn sau khi xử lý
 
-            return RedirectToPage("CategoriesListHidden");
-
             return RedirectToPage("StoreList");
+
+           
 
         }
 
